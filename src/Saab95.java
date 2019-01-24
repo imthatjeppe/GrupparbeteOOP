98b5d78acefe8b25ca8461d04fbd72b6cef13cfe
ModeDerp <modederp@hotmail.com>
2019-01-24 15:32:04 +0100

    hej

--- src/Saab95.java
+++ src/Saab95.java
@@ -4,37 +4,10 @@ public class Saab95 extends car{
 
 	public boolean turboOn;
 
-	public double enginePower; // Engine power of the car
-	public double currentSpeed; // The current speed of the car
-	public Color color; // Color of the car
-	public String modelName; // The car model name
-
+	
 	public Saab95() {
-		color = Color.BLACK;
-		enginePower = 125;
-		turboOn = false;
-		modelName = "Saab95";
+		super(Color.RED, 125, "Saab95");
 		stopEngine();
-	}
-
-	public double getEnginePower() {
-		return enginePower;
-	}
-
-	public double getCurrentSpeed() {
-		return currentSpeed;
-	}
-
-	public Color getColor() {
-		return color;
-	}
-
-	public void startEngine() {
-		currentSpeed = 0.1;
-	}
-
-	public void stopEngine() {
-		currentSpeed = 0;
 	}
 
 	public void setTurboOn() {
@@ -52,19 +25,4 @@ public class Saab95 extends car{
 		return enginePower * 0.01 * turbo;
 	}
 
-	public void incrementSpeed(double amount) {
-		currentSpeed = getCurrentSpeed() + speedFactor() * amount;
-	}
-
-	public void decrementSpeed(double amount) {
-		currentSpeed = getCurrentSpeed() - speedFactor() * amount;
-	}
-
-	public void gas(double amount) {
-		incrementSpeed(amount);
-	}
-
-	public void brake(double amount) {
-		decrementSpeed(amount);
-	}
 }
--- src/car.java
+++ src/car.java
@@ -1,4 +1,52 @@
+import javafx.scene.paint.Color;
 
-public class car{
+public abstract class car{
+	
+	public double enginePower; // Engine power of the car
+	public double currentSpeed; // The current speed of the car
+	public Color color; // Color of the car
+	public String modelName; // The car model name
+	
+	public car(Color c, double ep, String m) {
+		
+	}
+
+	public double getEnginePower() {
+		return enginePower;
+	}
+
+	public double getCurrentSpeed() {
+		return currentSpeed;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void startEngine() {
+		currentSpeed = 0.1;
+	}
+
+	public void stopEngine() {
+		currentSpeed = 0;
+	}
+
+	public abstract double speedFactor();
+
+	public void incrementSpeed(double amount) {
+		currentSpeed = getCurrentSpeed() + speedFactor() * amount;
+	}
+
+	public void decrementSpeed(double amount) {
+		currentSpeed = getCurrentSpeed() - speedFactor() * amount;
+	}
+
+	public void gas(double amount) {
+		incrementSpeed(amount);
+	}
+
+	public void brake(double amount) {
+		decrementSpeed(amount);
+	}
 	
 }
