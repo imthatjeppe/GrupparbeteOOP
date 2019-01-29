04699faef0d78b11d63fd9faf9aef66c8fc83b54
ModeDerp <modederp@hotmail.com>
2019-01-29 19:10:46 +0100

    Javadoc

--- src/Movable.java
+++ src/Movable.java
@@ -1,9 +1,18 @@
 
 public interface Movable {
 
+		/**
+		 * Ändrar bilens x & y koordinater beroende på dess riktning och speed
+		 */
 		void move();
 		
+		/**
+		 * Ändrar på bilens riktning negativt i intervallet 1 till 4
+		 */
 		void turnleft();
 		
+		/**
+		 * Ändrar på bilens riktning positivt i intervallet 1 till 4
+		 */
 		void turnright();
 }
--- src/car.java
+++ src/car.java
@@ -9,52 +9,95 @@ public abstract class car implements Movable{
 	private int x = 0;
 	private int y = 0;
 	private int dir = 1;
-
+	
+	/**
+	 * Konstruktor för att skapa en bil
+	 * @param c anger färgen bilen ska ha
+	 * @param ep anger motorns kraft
+	 * @param m anger bilens modell
+	 */
 	public car(Color c, double ep, String m) {
 		color = c;
 		enginePower = ep;
 		modelName = m;
 		currentSpeed = 0;
 	}
-
+	
+	/**
+	 * getEnginePower returnerar enginePower för den angivna bilen
+	 * @return double
+	 */
 	public double getEnginePower() {
 		return enginePower;
 	}
 
+	/**
+	 * getCurrentSpeed returnerar currentSpeed för den angivna bilen
+	 * @return double
+	 */
 	public double getCurrentSpeed() {
 		return currentSpeed;
 	}
 
+	/**
+	 * getColor returnerar Color för den angivna bilen
+	 * @return Color
+	 */
 	public Color getColor() {
 		return color;
 	}
-
+	
+	/**
+	 * startEngine startar bilen och ger den en currentSpeed på 0.1
+	 */
 	public void startEngine() {
 		currentSpeed = 0.1;
 	}
 
+	/**
+	 * startEngine stoppar bilen och ger den en currentSpeed på 0
+	 */
 	public void stopEngine() {
 		currentSpeed = 0;
 	}
 
 	public abstract double speedFactor();
 
+	/**
+	 * incrementSpeed ökar currentSpeed baserat på den individuella bilmodellens speedfactor och det angivna talet
+	 * @param amount anger hur mycket currentSpeed ska öka
+	 */
 	public void incrementSpeed(double amount) {
 		currentSpeed = getCurrentSpeed() + speedFactor() * amount;
 	}
 
+	/**
+	 * incrementSpeed minskar currentSpeed baserat på den individuella bilmodellens speedfactor och det angivna talet
+	 * @param amount anger hur mycket currentSpeed ska minska
+	 */
 	public void decrementSpeed(double amount) {
 		currentSpeed = getCurrentSpeed() - speedFactor() * amount;
 	}
 
+	/**
+	 * Metoden kallar på incrementSpeed för att öka currentSpeed
+	 * @param amount anger hur mycket som den ska öka med
+	 */
 	public void gas(double amount) {
 		incrementSpeed(amount);
 	}
 
+	/**
+	 * Metoden kallar på decementSpeed för att minska currentSpeed
+	 * @param amount anger hur mycket som den ska minska med
+	 */
 	public void brake(double amount) {
 		decrementSpeed(amount);
 	}
-	@Override
+	
+	/**
+	 * Metoden förflyttar bilens x & y koordinater beroende på dens riktning och currentSpeed
+	 */
 	public void move() {
 		switch(dir) {
 			case 1 :
@@ -75,7 +118,9 @@ public abstract class car implements Movable{
 		}
 	}
 
-	@Override
+	/**
+	 * Ändrar bilens riktning genom att minska variabeln dir med 1, om den är 1 börjar den om på 4
+	 */
 	public void turnleft() {
 		if(dir == 1) {
 			dir = 4;
@@ -85,7 +130,9 @@ public abstract class car implements Movable{
 		}
 	}
 
-	@Override
+	/**
+	 * Ändrar bilens riktning genom att öka variabeln dir med 1, om den är 4 börjar den om på 1
+	 */
 	public void turnright() {
 		if(dir == 4) {
 			dir = 1;
