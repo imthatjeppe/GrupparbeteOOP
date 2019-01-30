0d75d243a2642deee35e160cc738e170c066d801
imthatjeppe <jesper.johansson.te16c@gmail.com>
2019-01-30 12:20:06 +0100

    La till Javadoc för scania

--- src/Scania.java
+++ src/Scania.java
@@ -4,15 +4,28 @@ public class Scania extends car{
 
 	public int flatbedAngle = 0;
 
+	/**
+	 * Det är en konstruktor för Scania lastbilar
+	 * @param c lastbilens färg
+	 * @param ep engine power
+	 * @param m modell namn
+	 */
 	public Scania(Color c, double ep, String m) {
 		super(Color.WHITE, 750, "Scania");
 		stopEngine();
 	}
-
+	
+	/**
+	 * Returnernar hastighetsfaktorn för den individuella modellen.
+	 */
 	public double speedFactor() {
 		return enginePower * 0.01;
 	}
 
+	/**
+	 * Höjer flatbed och förändrar flatbed positivt
+	 * @param amount Hur mycket det ändras
+	 */
 	public void higherFlatbed(int amount){
 		if(currentSpeed > 0) return;
 		if(amount < 0) return;
@@ -23,7 +36,11 @@ public class Scania extends car{
 			flatbedAngle += amount;
 		}
 	}
-
+	
+	/**
+	 * Sänker flatbed och förändrar flatbed negativt
+	 * @param amount Hur mycket det ändras
+	 */
 	public void lowerFlatbed(int amount){
 		if(currentSpeed > 0) return;
 		if(amount < 0) return;
