d4594b3e47ddf49071c5b96d932dc78e2cae73c0
Grodlund <isak.gronlund.te16c@gmail.com>
2019-01-30 12:14:20 +0100

    lade till olika villkor för hur flaket beter sig på Scania

--- src/Scania.java
+++ src/Scania.java
@@ -2,6 +2,8 @@ import javafx.scene.paint.Color;
 
 public class Scania extends car{
 
+	public int flatbedAngle = 0;
+
 	public Scania(Color c, double ep, String m) {
 		super(Color.WHITE, 750, "Scania");
 		stopEngine();
@@ -9,5 +11,27 @@ public class Scania extends car{
 
 	public double speedFactor() {
 		return enginePower * 0.01;
+	}
+
+	public void higherFlatbed(int amount){
+		if(currentSpeed > 0) return;
+		if(amount < 0) return;
+		if(flatbedAngle + amount > 70){
+			flatbedAngle = 70;
+		}
+		else{
+			flatbedAngle += amount;
+		}
+	}
+
+	public void lowerFlatbed(int amount){
+		if(currentSpeed > 0) return;
+		if(amount < 0) return;
+		if(flatbedAngle - amount < 0){
+			flatbedAngle = 0;
+		}
+		else{
+			flatbedAngle -= amount;
+		}
 	}
 }
