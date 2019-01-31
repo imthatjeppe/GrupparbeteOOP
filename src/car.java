9d353ef690016045404cf0d3763b7d3c3a1f4f3d
ModeDerp <modederp@hotmail.com>
2019-01-31 15:30:30 +0100

    Skapade abstrakta klassen vehicle

--- src/car.java
+++ src/car.java
@@ -1,6 +1,6 @@
 import javafx.scene.paint.Color;
 
-public abstract class car implements Movable{
+public abstract class car extends vehicle implements Movable{
 
 	protected double enginePower; // Engine power of the car
 	protected double currentSpeed; // The current speed of the car
