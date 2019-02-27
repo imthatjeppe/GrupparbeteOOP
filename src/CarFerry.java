d48c70e4fa26c755b1ca5c02a297efd27fae329e
imthatjeppe <jesper.johansson.te16c@gmail.com>
2019-02-27 12:05:56 +0100

    Jahopp

--- src/CarFerry.java
+++ src/CarFerry.java
@@ -8,20 +8,30 @@ public class CarFerry extends Boat {
 	}
 	
 	private VehicleLoaderDelegate vehicleLoader;
-	
+	/**
+	 * Lastar bilar på båten
+	 * @param v Man säger vilket fordon som ska lastas
+	 */
 	public void loadVehicle(Vehicle v) {
 		vehicleLoader.loadVehicle(v, this.getModelName());
 	}
-	
+	/**
+	 * Lastar av ett fordon från båten
+	 * @return ett fordon
+	 */
 	public Vehicle unloadCar() {
 		return vehicleLoader.unloadFirstVehicle();
 	}
-
+   /**
+    * Returnerar fart faktorn
+    */
 	@Override
 	public double speedFactor() {
 		return enginePower * 0.01;
 	}
-	
+	/**
+	 * Sätter fordonets position.
+	 */
 	@Override
 	public void move() {
 		super.move();
