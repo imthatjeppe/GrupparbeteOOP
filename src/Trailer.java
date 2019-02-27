0135bbb4ceab6edbae57b8d22a54287c0b49be6f
imthatjeppe <jesper.johansson.te16c@gmail.com>
2019-02-27 12:17:15 +0100

    Update Trailer.java

--- src/Trailer.java
+++ src/Trailer.java
@@ -3,16 +3,27 @@ import javafx.scene.paint.Color;
 public abstract class Trailer extends GameObject{
 	
 	private Color color;
-
+	/**
+	 * konstruktor för trailer
+	 * @param c Färg på trailer
+	 * @param m modelnamnet
+	 */
 	public Trailer(Color c, String m) {
 		super(m);
 		color = c;
 	}
-	
+	/**
+	 * Hämtar färgen
+	 * @return returnar färgen
+	 */
 	public Color getColor() {
 		return this.color;
 	}
-
+	/**
+	 * Uppdaterar position på trailern
+	 * @param x  x koordinatern
+	 * @param y  y koordinatern
+	 */
 	public void updatePosition(double x, double y) {
 		setX(x);
 		setY(y);
