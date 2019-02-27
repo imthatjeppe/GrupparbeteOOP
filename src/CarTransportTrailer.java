50a119e426d79424b14f0a1314ae2f87677d23f4
imthatjeppe <jesper.johansson.te16c@gmail.com>
2019-02-27 12:11:03 +0100

    Update CarTransportTrailer.java

--- src/CarTransportTrailer.java
+++ src/CarTransportTrailer.java
@@ -2,21 +2,33 @@
 import javafx.scene.paint.Color;
 
 public class CarTransportTrailer extends Trailer {
-
+	
+	/**
+	 * Konstruktor för trailer
+	 * @param c Färgen på trailern
+	 */
 	public CarTransportTrailer(Color c) {
 		super(c, "CarTransportTrailer");
 	}
 	
 	private VehicleLoaderDelegate vehicleLoader;
-	
+	/**
+	 * Lastar bilar
+	 * @param v Modelnamnet
+	 */
 	public void loadCar(Vehicle v) {
 		vehicleLoader.loadVehicle(v, this.getModelName());
 	}
-	
+	/**
+	 * lastar av bilar
+	 * @return returnerar vilket fordon som lastats av.
+	 */
 	public Vehicle unloadCar() {
 		return vehicleLoader.unloadLastVehicle();
 	}
-	
+	/**
+	 * sätter positionen på fordonet.
+	 */
 	@Override
 	public void updatePosition(double x, double y) {
 		super.updatePosition(x, y);
